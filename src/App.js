16695d9be075ceab3dc6dc0597516a93176e3e59
gudetama <daniellewwise@gmail.com>
2021-03-14 23:56:24 -0400

    setting up logic for displaying questions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      hello quiz goes here
+    <div className="question-text">
+      {questions[0].questionText}
+      quiz goes here
+      <div className="answer-section">{questions[0].answerOptions.map((answerOption, index) =>
+        (<button>{answerOptions.answerText}</button>))}
+      </div>
     </div>
+    
   );
 }
 
