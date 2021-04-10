a1f74e7703a0c59f2a9b4e8bc22902706157e2fd
gudetama <daniellewwise@gmail.com>
2021-04-09 23:39:50 -0400

    testing Confetti component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import Confetti from "./components/Confetti";
 
 function App() {
-
-
   return (
-    <div className="question-text">
-      {questions[0].questionText}
-      quiz goes here
-      <div className="answer-section">{questions[0].answerOptions.map((answerOption, index) =>
-        (<button>{answerOptions.answerText}</button>))}
+    <div>
+      <Confetti/>
+      <div className="question-text">
+        {/* {questions[0].questionText} */}
+        quiz goes here
+        <div className="answer-section">
+          {/* {questions[0].answerOptions.map((answerOption, index) => (
+          <button>{answerOptions.answerText}</button>
+        ))} */}
+        </div>
       </div>
     </div>
-    
   );
 }
 
