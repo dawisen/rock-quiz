7eb764f403ec2a83945e6d1b3ffb4b9c2394d39c
gudetama <daniellewwise@gmail.com>
2021-04-12 23:32:31 -0400

    added QuestionList component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import "./App.css";
 import React from "react";
 import Confetti from "./components/Confetti";
+import QuestionList from "./components/QuestionList"
 
 function App() {
+  const [showScore, setShowScore] = useState(false);
+
   return (
     <div>
-      <Confetti/>
-      <div className="question-text">
-        {/* {questions[0].questionText} */}
-        quiz goes here
-        <div className="answer-section">
-          {/* {questions[0].answerOptions.map((answerOption, index) => (
-          <button>{answerOptions.answerText}</button>
-        ))} */}
-        </div>
+      <Confetti />
+      <QuestionList showScore={showScore} setShowScore={setShowScore} />
+      <div className="app">
+        {showScore ? (
+          <div className="score-section">// ... score section markup</div>
+        ) : (
+          
+        )}
       </div>
     </div>
   );
