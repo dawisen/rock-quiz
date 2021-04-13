48cc5988b3805d8524f4e668caf90e510e95e8a9
gudetama <daniellewwise@gmail.com>
2021-04-12 23:34:10 -0400

    Created an array to store quesion data

---
+++ src/questions.js
@@ -0,0 +1,4 @@
+const questions = [{
+  questionText: 'question 1', answerOptions: ['answer 1', 'answer 2', 'answer 3']}, {questionText: 'question 2', answerOptions: ['answer 1', 'answer 2', 'answer 3']}, {questionText: 'question 3', answerOptions: ['answer 1', 'answer 2', 'answer 3']}];
+
+export default questions;
